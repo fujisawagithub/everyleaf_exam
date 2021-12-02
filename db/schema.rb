d8b2f4e53b11ad28b295bebd7ea5cfdbad29b9af
yuki fujisawa <fujisawa.yuki@outlook.com>
2021-12-02 18:04:20 +0900

    tasksテーブル作成

---
+++ db/schema.rb
@@ -0,0 +1,13 @@
+ActiveRecord::Schema.define(version: 2021_12_02_085047) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "tasks", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+end
