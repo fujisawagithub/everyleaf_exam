e016fbf3109b8c8d1c58f74c212ff84ad75e0307
yuki fujisawa <fujisawa.yuki@outlook.com>
2021-12-03 17:42:40 +0900

    system_specテスト追加

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -1,21 +1,33 @@
 require 'rails_helper'
-describe 'タスク管理機能', type: :system do
+RSpec.describe 'タスク管理機能', type: :system do
+  describe '新規作成機能' do
+    context 'タスクを新規作成した場合' do
+      it '作成したタスクが表示される' do
+        visit new_task_path
+        fill_in 'タスク名', with: 'step1'
+        fill_in '詳細', with: 'step1やる'
+        click_on '登録'
+        expect(page).to have_content 'step1'
+        expect(page).to have_content 'step1やる'
+      end
+    end
+  end
   describe '一覧表示機能' do
     context '一覧画面に遷移した場合' do
       it '作成済みのタスク一覧が表示される' do
-        # テストで使用するためのタスクを作成
         task = FactoryBot.create(:task, title: 'task')
-        # タスク一覧ページに遷移
         visit tasks_path
-        # ページ遷移直後でデバッグ
-        # タスク一覧ページに遷移できているかを確認
-        current_path
-        # タスクがデータベースに作成されているかを確認
-        Task.count
-        # 表示するHTMLにタスク情報が入っているかを確認
-        page.html
         expect(page).to have_content 'task'
       end
     end
+  end
+  describe '詳細表示機能' do
+     context '任意のタスク詳細画面に遷移した場合' do
+       it '該当タスクの内容が表示される' do
+        @task = FactoryBot.create(:task, title: 'task')
+        visit task_path(@task)
+        expect(page).to have_content 'task'
+       end
+     end
   end
 end
