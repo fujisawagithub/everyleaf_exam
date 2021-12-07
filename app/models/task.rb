cdbc732fadbf64d4d56589f4293a1ea2e1a9726d
yuki fujisawa <fujisawa.yuki@outlook.com>
2021-12-07 18:21:03 +0900

    indexに検索機能追加

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,7 @@
 class Task < ApplicationRecord
   validates :title,  presence: true, length: { maximum: 10 }
   validates :content,  presence: true, length: { maximum: 255 }
+  validates :deadline, presence: true
+  validates :status, presence: true
+  enum status: {未着手: 0, 着手中: 1, 完了: 2}
 end
