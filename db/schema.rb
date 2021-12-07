3699ce1e17729db61b1801205587283b6d2b82bc
yuki fujisawa <fujisawa.yuki@outlook.com>
2021-12-07 17:52:39 +0900

    statusカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_07_014221) do
+ActiveRecord::Schema.define(version: 2021_12_07_084959) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,9 @@ ActiveRecord::Schema.define(version: 2021_12_07_014221) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.date "deadline", default: -> { "CURRENT_DATE" }, null: false
+    t.integer "status", default: 0, null: false
+    t.index ["status"], name: "index_tasks_on_status"
+    t.index ["title"], name: "index_tasks_on_title"
   end
 
 end
