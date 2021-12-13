026239a887ebae1c55b6bf2474cf3beb9d94d325
yuki fujisawa <fujisawa.yuki@outlook.com>
2021-12-13 12:28:42 +0900

    検索機能System_Spec追加

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,5 @@
 class Task < ApplicationRecord
-  validates :title,  presence: true, length: { maximum: 10 }
+  validates :title,  presence: true, length: { maximum: 20 }
   validates :content,  presence: true, length: { maximum: 255 }
   validates :deadline, presence: true
   validates :status, presence: true
--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -4,20 +4,53 @@ RSpec.describe 'タスク管理機能', type: :system do
   before do
     visit tasks_path
   end
+
+    describe '検索機能' do
+      context 'タイトルであいまい検索をした場合' do
+        it "検索キーワードを含むタスクで絞り込まれる" do
+          visit tasks_path
+          fill_in 'タスク名', with: 'タイトル1'
+          click_on 'Search'
+          expect(page).to have_content 'タイトル1'
+        end
+      end
+      context 'ステータス検索をした場合' do
+        it "ステータスに完全一致するタスクが絞り込まれる" do
+         visit tasks_path
+         select '未着手', from: 'ステータス'
+         click_on 'Search'
+         expect(page).to have_content '未着手'
+        end
+      end
+      context 'タイトルのあいまい検索とステータス検索をした場合' do
+        it "検索キーワードをタイトルに含み、かつステータスに完全一致するタスク絞り込まれる" do
+          visit tasks_path
+          fill_in 'タスク名', with: 'タイトル1'
+          select '未着手', from: 'ステータス'
+          click_on 'Search'
+          expect(page).to have_content 'タイトル1'
+          expect(page).to have_content '未着手'
+        end
+      end
+    end
   describe '新規作成機能' do
     context 'タスクを新規作成した場合' do
       it '作成したタスクが表示される' do
         visit new_task_path
-        fill_in 'task[title]', with: 'step1'
-        fill_in 'task[content]', with: 'step1をクリアする'
-        fill_in 'task[deadline]', with: '002021-12-01'
+        fill_in 'task[title]', with: 'step3'
+        fill_in 'task[content]', with: 'step3をクリアする'
+        fill_in 'task[deadline]', with: DateTime.now
+        select '未着手', from:'task_status'
         click_on '登録'
-        expect(page).to have_content 'step1'
-        expect(page).to have_content 'step1をクリアする'
-        expect(page).to have_content '2021-12-01'
+        expect(page).to have_content 'step3'
+        expect(page).to have_content 'step3をクリアする'
+        expect(page).to have_content (DateTime.now).strftime('%Y-%m-%d')
+        expect(page).to have_content '未着手'
       end
     end
   end
+
+  
   describe '一覧表示機能' do
     context '一覧画面に遷移した場合' do
       it '作成済みのタスク一覧が表示される' do
