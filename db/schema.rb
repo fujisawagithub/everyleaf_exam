1ba75f88a0d11cd2339876ad5aa93c07cd0fdd9c
yuki fujisawa <fujisawa.yuki@outlook.com>
2021-12-13 12:53:16 +0900

    add_priority_to_tasks

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_07_084959) do
+ActiveRecord::Schema.define(version: 2021_12_13_035115) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2021_12_07_084959) do
     t.datetime "updated_at", precision: 6, null: false
     t.date "deadline", default: -> { "CURRENT_DATE" }, null: false
     t.integer "status", default: 0, null: false
+    t.integer "priority", default: 0, null: false
     t.index ["status"], name: "index_tasks_on_status"
     t.index ["title"], name: "index_tasks_on_title"
   end
