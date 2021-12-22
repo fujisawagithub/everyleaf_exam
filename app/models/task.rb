b414c232e92442b1634f07faa33854c7bf00ea97
yuki fujisawa <fujisawa.yuki@outlook.com>
2021-12-22 10:17:42 +0900

    ラベル機能用テーブル作成

--- app/models/task.rb
+++ app/models/task.rb
@@ -5,6 +5,8 @@ class Task < ApplicationRecord
   validates :status, presence: true
   validates :priority, presence: true
   belongs_to :user
+  has_many :labellings, dependent: :destroy
+  has_many :labels, through: :labellings
   enum status: { 未着手: 0, 着手中: 1, 完了: 2 }
   enum priority: { 高: 0, 中: 1, 低: 2 }
 
